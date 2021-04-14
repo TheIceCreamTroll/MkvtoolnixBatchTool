c64b7136f9314bf49cb3fed6914998bf17b6cc91
ls3485 <ls3485@drexel.edu>
2021-04-14 01:49:40 -0400

    first commit

---
+++ MkvtoolnixBatchv2.bat
@@ -0,0 +1,31 @@
+@echo off
+REM Unencoded powershell:
+REM $index = (Get-Content options.json | Select-Object -Index ((Get-Content options.json).Length - 4))
+REM (Get-Content options.json).Replace('"(",', "").Replace('")",', "").Replace($index, "") | Select-String -NotMatch -pattern '.mkv"', '"--output",', '"--title",' | Out-File options.json -Force -Encoding "UTF8"
+
+
+set mkvmerge="C:\Program Files\MKVToolNix\mkvmerge.exe"
+if not exist "options.json" (
+    echo Options file 'options.json' not found. Exiting.
+    pause>nul
+    exit
+)
+
+
+find "--output" options.json>NUL
+if errorlevel 1 (goto NOTFOUND) else goto FOUND)
+
+
+:FOUND
+powershell -EncodedCommand CgAkAGkAbgBkAGUAeAAgAD0AIAAoAEcAZQB0AC0AQwBvAG4AdABlAG4AdAAgAG8AcAB0AGkAbwBuAHMALgBqAHMAbwBuACAAfAAgAFMAZQBsAGUAYwB0AC0ATwBiAGoAZQBjAHQAIAAtAEkAbgBkAGUAeAAgACgAKABHAGUAdAAtAEMAbwBuAHQAZQBuAHQAIABvAHAAdABpAG8AbgBzAC4AagBzAG8AbgApAC4ATABlAG4AZwB0AGgAIAAtACAANAApACkACgAKACgARwBlAHQALQBDAG8AbgB0AGUAbgB0ACAAbwBwAHQAaQBvAG4AcwAuAGoAcwBvAG4AKQAuAFIAZQBwAGwAYQBjAGUAKAAnACIAKAAiACwAJwAsACAAIgAiACkALgBSAGUAcABsAGEAYwBlACgAJwAiACkAIgAsACcALAAgACIAIgApAC4AUgBlAHAAbABhAGMAZQAoACQAaQBuAGQAZQB4ACwAIAAiACIAKQAgAHwAIABTAGUAbABlAGMAdAAtAFMAdAByAGkAbgBnACAALQBOAG8AdABNAGEAdABjAGgAIAAtAHAAYQB0AHQAZQByAG4AIAAnAC4AbQBrAHYAIgAnACwAIAAnACIALQAtAG8AdQB0AHAAdQB0ACIALAAnACwAIAAnACIALQAtAHQAaQB0AGwAZQAiACwAJwAgAHwAIABPAHUAdAAtAEYAaQBsAGUAIABvAHAAdABpAG8AbgBzAC4AagBzAG8AbgAgAC0ARgBvAHIAYwBlACAALQBFAG4AYwBvAGQAaQBuAGcAIAAiAFUAVABGADgAIgAKAA==
+
+
+:NOTFOUND
+if not exist "mkvmerge_out" (mkdir "mkvmerge_out")
+for %%f in (*.mkv) do %mkvmerge% @options.json -o "mkvmerge_out/%%f" "%%f"
+echo.
+echo ============================
+echo Done. Press any key to exit.
+pause>nul
+exit
+
